7c039c4557d1be8f2e787f5720dac6e6935041a6
456 <ryzalen@yandex.ru>
2025-11-04 00:25:19 +0400

    Add contracts

---
+++ Domain/Contracts/IDoctorRepository.cs
@@ -0,0 +1,14 @@
+﻿using policlinicApp.Domain.Entities;
+
+namespace policlinicApp.Domain.Entities;
+
+public interface IDoctorRepository
+{
+    Doctor GetByPassport(string passportNumber);
+    IEnumerable<Doctor> GetAll();
+    IEnumerable<Doctor> GetDoctorsBySpecialization(int specializationId);
+    IEnumerable<Doctor> GetDoctorsWithExperience(int minExperienceYears);
+    void Add(Doctor doctor);
+    void Update(Doctor doctor);
+    void Delete(string passportNumber);
+}
