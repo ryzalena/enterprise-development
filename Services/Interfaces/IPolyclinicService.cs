8f75780fd2bccdc1a5ce8a670c3e3f086e18fdb6
456 <ryzalen@yandex.ru>
2025-11-04 00:25:50 +0400

    Add Interfaces

---
+++ Services/Interfaces/IPolyclinicService.cs
@@ -0,0 +1,12 @@
+﻿using policlinicApp.Domain.Entities;
+
+namespace policlinicApp.Services.Interfaces;
+
+public interface IPolyclinicService
+{
+    IEnumerable<Doctor> GetDoctorsWithExperience(int minExperienceYears);
+    IEnumerable<Patient> GetPatientsByDoctorOrderedByName(string doctorPassport);
+    int GetFollowUpAppointmentsCountLastMonth();
+    IEnumerable<Patient> GetPatientsOver30WithMultipleDoctors();
+    IEnumerable<Appointment> GetAppointmentsInRoomForCurrentMonth(string roomNumber);
+}
