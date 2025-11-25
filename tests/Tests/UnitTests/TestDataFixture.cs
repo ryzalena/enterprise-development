7d91fa9f88174d0f56db667a6fa8eccdf16aaa5d
456 <ryzalen@yandex.ru>
2025-11-25 12:32:21 +0400

    Вынесено в отдельный файл

---
+++ tests/Tests/UnitTests/TestDataFixture.cs
@@ -0,0 +1,31 @@
+﻿// TestDataFixture.cs
+using Domain.Entities;
+using Domain.TestData;
+
+namespace Tests.UnitTests;
+
+/// <summary>
+/// Фикстура для предоставления тестовых данных
+/// </summary>
+public class TestDataFixture
+{
+    /// <summary>
+    /// Список пациентов
+    /// </summary>
+    public List<Patient> Patients { get; } = TestData.Patients;
+
+    /// <summary>
+    /// Список врачей
+    /// </summary>
+    public List<Doctor> Doctors { get; } = TestData.Doctors;
+
+    /// <summary>
+    /// Список записей на прием
+    /// </summary>
+    public List<Appointment> Appointments { get; } = TestData.Appointments;
+
+    /// <summary>
+    /// Список специализаций
+    /// </summary>
+    public List<Specialization> Specializations { get; } = TestData.Specializations;
+}
