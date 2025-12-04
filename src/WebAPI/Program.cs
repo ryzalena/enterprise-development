b18328b537acbd717e0bdb30830d08d369a00f51
456 <ryzalen@yandex.ru>
2025-12-04 19:41:06 +0400

    исправлено для корректного отображения summary

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services;
+﻿using System.Reflection;
+using Application.Services;
 using Domain.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,13 +14,41 @@ builder.Services.AddSwaggerGen(options =>
         Version = "v1",
         Description = "API for Polyclinic Management System"
     });
+
+    // Добавляем XML-комментарии из основного проекта
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath);
+    }
+
+    // Если DTO находятся в отдельном проекте Application, добавляем и его XML
+    try
+    {
+        var dtoAssembly = typeof(Application.Dtos.AppointmentDto).Assembly;
+        var dtoXmlFile = $"{dtoAssembly.GetName().Name}.xml";
+        var dtoXmlPath = Path.Combine(AppContext.BaseDirectory, dtoXmlFile);
+        
+        if (File.Exists(dtoXmlPath))
+        {
+            options.IncludeXmlComments(dtoXmlPath);
+        }
+    }
+    catch
+    {
+        // Если не удалось найти XML для DTO, просто продолжаем
+    }
 });
 
+// Регистрация сервисов
 builder.Services.AddScoped<IDoctorService, DoctorService>();
 builder.Services.AddScoped<IPatientService, PatientService>();
 builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 builder.Services.AddScoped<ISpecializationService, SpecializationService>();
 
+// Настройка CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -32,6 +61,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Middleware pipeline
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
